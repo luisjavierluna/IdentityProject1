c12230de610700b79fdad98702990658d257dd89
luisjavierluna <javierlunaluis@gmail.com>
2022-12-01 15:56:51 -0600

    16. Crear Controlador y Registro de Usuario

---
+++ IdentityProject1/Controllers/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using IdentityProject1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject1.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SignIn()
+        {
+            SignInViewModel signInViewModel = new SignInViewModel();
+            return View(signInViewModel);
+        }
+    }
+}
